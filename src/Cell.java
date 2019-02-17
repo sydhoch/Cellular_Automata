5cab62653f3c5619f6a6be7727f919cf62c82693
SYDNEY HOCHBERG <srh57@duke.edu>
2019-02-17 15:05:04 -0500

    added color features to Cell

--- src/Cell.java
+++ src/Cell.java
@@ -4,9 +4,13 @@ import java.util.Objects;
 public abstract class Cell extends Object{
     private int myState;
     private Paint myColor;
+    private static String zeroColor = "BLUE";
+    private static String oneColor = "RED";
+    private static String twoColor = "YELLOW";
 
     public Cell(int state){
         myState = state;
+        changeColor(state);
     }
 
     public int getState(){
@@ -27,5 +31,18 @@ public abstract class Cell extends Object{
 
     private void setColor(String color){
         myColor.valueOf(color);
+    }
+
+    private void changeColor(int state){
+        if(state==0){
+            setColor(zeroColor);
+        }
+        else if(state==1){
+            setColor(oneColor);
+        }
+        else{
+            setColor(twoColor);
+        }
+
     }
 }
