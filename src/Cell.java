286e115fb0355bd6c76b11aadce225c59db9e4c8
SYDNEY HOCHBERG <srh57@duke.edu>
2019-02-17 14:53:57 -0500

    set up cell, perccell, and golcell, and created state class but did not start working on it yet

--- src/Cell.java
+++ src/Cell.java
@@ -1,5 +1,31 @@
-public abstract class Cell {
-    //checkNeighborStatus
-    //getState
-    //changeState
+import javafx.scene.paint.Paint;
+import java.util.Objects;
+
+public abstract class Cell extends Object{
+    private int myState;
+    private Paint myColor;
+
+    public Cell(int state){
+        myState = state;
+    }
+
+    public int getState(){
+        return myState;
+    }
+
+    public abstract void checkNeighborStatus(Object[] neighbors);
+
+    public abstract void changeState();
+
+    protected void setState(int state){
+        myState = state;
+    }
+
+    protected boolean equals(Cell o) {
+        return this.getState() == o.getState();
+    }
+
+    private void setColor(String color){
+        myColor.valueOf(color);
+    }
 }
--- src/GoLCell.java
+++ src/GoLCell.java
@@ -1,3 +1,45 @@
 public class GoLCell extends Cell {
+    //state == 0, dead
+    //state == 1, alive
+    public GoLCell(int state) {
+        super(state);
+    }
+
+    @Override
+    public void checkNeighborStatus(Object[] neighbors){
+        int neighborsAlive = getNumOfAliveNeighbors(neighbors);
+        if(getState()==1){
+            if(neighborsAlive<2 || neighborsAlive>3){
+                changeState();
+            }
+        }
+        if(getState()==0){
+            if(neighborsAlive==3){
+                changeState();
+            }
+        }
+    }
+
+    @Override
+    public void changeState(){
+        if(getState()==1){
+            setState(0);
+        }
+        else{
+            setState(1);
+        }
+
+    }
+
+    private int getNumOfAliveNeighbors(Object[] neighbors){
+        int aliveNeighbors=0;
+        for(int i=0;i<neighbors.length;i++){
+            if(neighbors[i].equals(1)) {
+                aliveNeighbors++;
+            }
+        }
+        return aliveNeighbors;
+    }
+
 
 }
