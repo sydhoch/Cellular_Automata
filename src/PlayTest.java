dd7118899de90c0048d6a08fd472ab1c499ca792
Sara Behn <srb71@duke.edu>
2019-02-20 15:39:35 -0500

    testing for setNeighbors

---
+++ src/PlayTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayTest {
+
+    @Test
+    void cellHeightCorrect() {
+
+    }
+
+    @Test
+    void cellWidthCorrect(){
+
+    }
+
+
+}
