1bb99fc0d2f890f5e4aa8ce995506cc8742744ed
SYDNEY HOCHBERG <srh57@duke.edu>
2019-02-21 15:19:34 -0500

    change colors of states in play to be more visible for perc

--- src/Play.java
+++ src/Play.java
@@ -21,9 +21,9 @@ public class Play extends Application {
     private static final String FILE_NAME = "perc-grid-2.csv";
     private static final String TITLE = "Cell Simulation";
     private static final int WINDOW_SIZE = 500;
-    private static Paint ZERO_COLOR = Color.BLUE;
-    private static Paint ONE_COLOR = Color.RED;
-    private static Paint TWO_COLOR = Color.YELLOW;
+    private static Paint ZERO_COLOR = Color.WHITE;
+    private static Paint ONE_COLOR = Color.BLUE;
+    private static Paint TWO_COLOR = Color.BLACK;
 
     private Scene myScene;
     private Group myRoot;
