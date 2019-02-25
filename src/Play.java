9df71e5e39527d57c70148412f30cea5a25efd2a
Sara Behn <srb71@duke.edu>
2019-02-24 21:17:08 -0500

    added sidebar and starter methods

--- src/Play.java
+++ src/Play.java
@@ -4,24 +4,31 @@ import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static javafx.scene.input.KeyCode.SPACE;
 
 
 public class Play extends Application {
 
 
+
+    private static final Paint BACKGROUND = Color.GREY;
+    private static final String FILE_NAME = "gol-grid-2.csv";
+    private static final String TITLE = "Cell Simulation";
     private static final int FRAMES_PER_SECOND = 1;
     private static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
     private static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
-    private static final Paint BACKGROUND = Color.GREY;
-    private static final String FILE_NAME = "perc-grid-2.csv";
-    private static final String TITLE = "Cell Simulation";
-    private static final int WINDOW_SIZE = 500;
+    private static final int SIM_SIZE = 500;
+    private static final int WINDOW_WIDTH = SIM_SIZE + 300;
     private static Paint ZERO_COLOR = Color.BLUE;
     private static Paint ONE_COLOR = Color.RED;
     private static Paint TWO_COLOR = Color.YELLOW;
@@ -29,20 +36,24 @@ public class Play extends Application {
     private Scene myScene;
     private Group myRoot;
     private Grid myGrid;
+    private boolean stepThroughMode;
+    private Timeline myAnimation;
+
 
 
     public void start(Stage stage) {
         myGrid = new Grid(FILE_NAME);
         myRoot = new Group();
-        myScene = setUpGame(WINDOW_SIZE, WINDOW_SIZE, BACKGROUND);
+        myScene = setUpGame(WINDOW_WIDTH, SIM_SIZE, BACKGROUND);
+        stepThroughMode = false;
         stage.setScene(myScene);
         stage.setTitle(TITLE);
         stage.show();
         var frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY), e -> step(SECOND_DELAY));
-        var animation = new Timeline();
-        animation.setCycleCount(Timeline.INDEFINITE);
-        animation.getKeyFrames().add(frame);
-        animation.play();
+        myAnimation = new Timeline();
+        myAnimation.setCycleCount(Timeline.INDEFINITE);
+        myAnimation.getKeyFrames().add(frame);
+        myAnimation.play();
     }
 
     private Scene setUpGame(int width, int height, Paint background) {
@@ -53,6 +64,7 @@ public class Play extends Application {
 
     private void displayStates() {
         myRoot.getChildren().clear();
+        setButtons();
         for (int i = 0; i < myGrid.getHeight(); i++) {
             for (int j = 0; j < myGrid.getWidth(); j++) {
                 myRoot.getChildren().add(setRectangle(i, j));
@@ -60,9 +72,41 @@ public class Play extends Application {
         }
     }
 
+    private void setButtons(){
+
+    }
+
+    private void load(){
+        myGrid = new Grid("");
+    }
+
+    private void pause(){
+        myAnimation.pause();
+    }
+
+    private void resume(){
+        myAnimation.play();
+    }
+
+    private void stepThrough(){
+
+    }
+
+    private void speedUp(){
+        myAnimation.setRate(2);
+    }
+
+    private void slowDown(){
+        myAnimation.setRate(.5);
+    }
+
+    private void resetSpeed(){
+        myAnimation.setRate(1);
+    }
+
     private Rectangle setRectangle(int i, int j) {
-        int cellHeight = WINDOW_SIZE/myGrid.getHeight();
-        int cellWidth = WINDOW_SIZE/myGrid.getWidth();
+        int cellHeight = SIM_SIZE /myGrid.getHeight();
+        int cellWidth = SIM_SIZE /myGrid.getWidth();
         Rectangle ret = new Rectangle(cellHeight*i, cellWidth*j, cellHeight, cellWidth);
         ret.setFill(setColor(myGrid.getCell(i, j).getState()));
         return ret;
@@ -104,5 +148,13 @@ public class Play extends Application {
             }
         }
     }
+
+    private void handleKeyInput(KeyCode code) {
+        if(code.equals(SPACE)){
+
+        }
+    }
+
+
 }
 
