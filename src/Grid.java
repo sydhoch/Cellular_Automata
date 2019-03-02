3d9c8ef2a744ef9cf53016f72889dab0543136c1
Sara Behn <srb71@duke.edu>
2019-03-02 17:09:37 -0500

    state map check --> runs all green

--- src/Grid.java
+++ src/Grid.java
@@ -32,18 +32,25 @@ public class Grid {
     protected void setNextStates() {
         for (int i = 0; i < myHeight; i++) {
             for (int j = 0; j < myWidth; j++) {
-                //Cell[] neighbors = setNeighborsToroidal(i, j);
-                //getCell(i, j).checkNeighborStatus(neighbors);
+                Cell[] neighbors = setNeighborsToroidal(i, j);
+                getCell(i, j).checkNeighborStatus(neighbors);
             }
         }
     }
 
     protected void updateStates() {
+        myCellStates.clear();
         for (int i = 0; i < myHeight; i++) {
             for (int j = 0; j < myWidth; j++) {
                 getCell(i, j).updateCell();
+                addToMap(i, j, getCell(i, j));
             }
         }
+    }
+
+    private void addToMap(int row, int col, Cell cell){
+        myCellStates.putIfAbsent(cell.getState(), new ArrayList<>());
+        myCellStates.get(cell.getState()).add(new Integer[]{row, col});
     }
 
 
@@ -51,74 +58,74 @@ public class Grid {
         return myGrid[row][col];
     }
 
-//    public Cell[] setNeighborsToroidal(int row, int col) { //break up
-//        Cell[] neighbors = new Cell[8];
-//        if (row == 0 && col == 0) {
-//            neighbors[0] = getCell(myHeight - 1, myWidth - 1);
-//        } else if (row == 0) {
-//            neighbors[0] = getCell(myHeight - 1, col - 1);
-//        } else if (col == 0) {
-//            neighbors[0] = getCell(row - 1, myWidth - 1);
-//        } else {
-//            neighbors[0] = getCell(row - 1, col - 1);
-//        }
-//
-//        if (row == 0) {
-//            neighbors[1] = getCell(myHeight - 1, col);
-//        } else {
-//            neighbors[1] = getCell(row - 1, col);
-//        }
-//
-//        if (row == 0 && col == myWidth - 1) {
-//            neighbors[2] = getCell(myHeight - 1, 0);
-//        } else if (row == 0) {
-//            neighbors[2] = getCell(myHeight - 1, col + 1);
-//        } else if (col == myWidth - 1) {
-//            neighbors[2] = getCell(row - 1, 0);
-//        } else {
-//            neighbors[2] = getCell(row - 1, col + 1);
-//        }
-//
-//        if (col == 0) {
-//            neighbors[3] = getCell(row, myWidth - 1);
-//        } else {
-//            neighbors[3] = getCell(row, col - 1);
-//        }
-//
-//        if (col == myWidth - 1) {
-//            neighbors[4] = getCell(row, 0);
-//        } else {
-//            neighbors[4] = getCell(row, col + 1);
-//        }
-//
-//        if (row == myHeight - 1 && col == 0) {
-//            neighbors[5] = getCell(0, myWidth - 1);
-//        } else if (row == myHeight - 1) {
-//            neighbors[5] = getCell(0, col - 1);
-//        } else if (col == 0) {
-//            neighbors[5] = getCell(row + 1, myWidth - 1);
-//        } else {
-//            neighbors[5] = getCell(row + 1, col - 1);
-//        }
-//
-//        if (row == myHeight - 1) {
-//            neighbors[6] = getCell(0, col);
-//        } else {
-//            neighbors[6] = getCell(row + 1, col);
-//        }
-//
-//
-//        if (row == myHeight - 1 && col == myWidth - 1) {
-//            neighbors[7] = getCell(0, 0);
-//        } else if (row == myHeight - 1) {
-//            neighbors[7] = getCell(0, col + 1);
-//        } else if (col == myWidth - 1) {
-//            neighbors[7] = getCell(row + 1, 0);
-//        } else {
-//            neighbors[7] = getCell(row + 1, col + 1);
-//        }
-//        return neighbors;
-//    }
+    public Cell[] setNeighborsToroidal(int row, int col) { //break up
+        Cell[] neighbors = new Cell[8];
+        if (row == 0 && col == 0) {
+            neighbors[0] = getCell(myHeight - 1, myWidth - 1);
+        } else if (row == 0) {
+            neighbors[0] = getCell(myHeight - 1, col - 1);
+        } else if (col == 0) {
+            neighbors[0] = getCell(row - 1, myWidth - 1);
+        } else {
+            neighbors[0] = getCell(row - 1, col - 1);
+        }
+
+        if (row == 0) {
+            neighbors[1] = getCell(myHeight - 1, col);
+        } else {
+            neighbors[1] = getCell(row - 1, col);
+        }
+
+        if (row == 0 && col == myWidth - 1) {
+            neighbors[2] = getCell(myHeight - 1, 0);
+        } else if (row == 0) {
+            neighbors[2] = getCell(myHeight - 1, col + 1);
+        } else if (col == myWidth - 1) {
+            neighbors[2] = getCell(row - 1, 0);
+        } else {
+            neighbors[2] = getCell(row - 1, col + 1);
+        }
+
+        if (col == 0) {
+            neighbors[3] = getCell(row, myWidth - 1);
+        } else {
+            neighbors[3] = getCell(row, col - 1);
+        }
+
+        if (col == myWidth - 1) {
+            neighbors[4] = getCell(row, 0);
+        } else {
+            neighbors[4] = getCell(row, col + 1);
+        }
+
+        if (row == myHeight - 1 && col == 0) {
+            neighbors[5] = getCell(0, myWidth - 1);
+        } else if (row == myHeight - 1) {
+            neighbors[5] = getCell(0, col - 1);
+        } else if (col == 0) {
+            neighbors[5] = getCell(row + 1, myWidth - 1);
+        } else {
+            neighbors[5] = getCell(row + 1, col - 1);
+        }
+
+        if (row == myHeight - 1) {
+            neighbors[6] = getCell(0, col);
+        } else {
+            neighbors[6] = getCell(row + 1, col);
+        }
+
+
+        if (row == myHeight - 1 && col == myWidth - 1) {
+            neighbors[7] = getCell(0, 0);
+        } else if (row == myHeight - 1) {
+            neighbors[7] = getCell(0, col + 1);
+        } else if (col == myWidth - 1) {
+            neighbors[7] = getCell(row + 1, 0);
+        } else {
+            neighbors[7] = getCell(row + 1, col + 1);
+        }
+        return neighbors;
+    }
 
     public Cell[] setNeighbors(int row, int col) {
         Neighborhood neighbors = new Neighborhood(row, col, myShape, myArr, myEdge);
--- src/gridTest.java
+++ src/gridTest.java
@@ -64,25 +64,32 @@ class GridTest {
 
     @Test
     void checkStateMapCorrect(){
-        Grid myGrid = new Grid("perc-grid-1.csv");
         List<Integer[]> expected0 = new ArrayList<>(Arrays.asList(new Integer[]{0,0}, new Integer[]{0,2}, new Integer[]{0,4}, new Integer[]{1,1}, new Integer[]{1,3}, new Integer[]{1,4}, new Integer[]{2,1}, new Integer[]{2,2}, new Integer[]{3,0}, new Integer[]{3,1}, new Integer[]{3,3}, new Integer[]{3,4}, new Integer[]{4,2}, new Integer[]{4,3}));
         List<Integer[]> expected1 = new ArrayList<>(Arrays.asList(new Integer[]{0,1}, new Integer[]{0,3}));
         List<Integer[]> expected2 = new ArrayList<>(Arrays.asList(new Integer[]{1,0}, new Integer[]{1,2}, new Integer[]{2,0}, new Integer[]{2,3}, new Integer[]{2,4}, new Integer[]{3,2}, new Integer[]{4,0}, new Integer[]{4,1}, new Integer[]{4,4}));
 
+        Grid myGrid = new Grid("perc-grid-1.csv");
+        myGrid.updateStates();
         List<Integer[]> actual0 = myGrid.getCellsInState(0);
         List<Integer[]> actual1 = myGrid.getCellsInState(1);
         List<Integer[]> actual2 = myGrid.getCellsInState(2);
 
-        assertEquals(expected0, actual0);
-        assertEquals(expected1, actual1);
-        assertEquals(expected2, actual2);
+        compareLists(expected0, actual0);
+        compareLists(expected1, actual1);
+        compareLists(expected2, actual2);
     }
 
     void compareCells(Grid myGrid, Cell[] expected, int row, int col){
-        //Cell[] actual = myGrid.setNeighborsToroidal(row, col);
+        Cell[] actual = myGrid.setNeighborsToroidal(row, col);
         for(int i = 0; i < 8; i++){
-            //assertEquals(expected[i].getState(), actual[i].getState());
+            assertEquals(expected[i].getState(), actual[i].getState());
         }
     }
 
+    void compareLists(List<Integer[]> expected, List<Integer[]> actual){
+        for(int i = 0; i < expected.size(); i++){
+            assertEquals(expected.get(i)[0], actual.get(i)[0]);
+            assertEquals(expected.get(i)[1], actual.get(i)[1]);
+        }
+    }
 }
